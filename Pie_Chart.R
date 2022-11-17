00b0c54585d5be64ca74b2b38e1c2d4976f765ab
HugoWinterGhost <hfief1806@gmail.com>
2022-11-17 16:54:56 +0100

    add ppt cible pie chart

--- Pie_Chart.R
+++ Pie_Chart.R
@@ -1,32 +1,39 @@
-default.payment.next.month <- read.csv(
+credit_card <- read.csv(
   "~/Documents/Cours/Machine Learning/R_Projet/UCI_Credit_Card.csv", 
   h = TRUE
 )
 
-default.payment.next.month[1:5, ]
+credit_card[1:5, ]
 
 
 # install.packages("dplyr")
 library(dplyr)
 
-EDUCATION_Count <- default.payment.next.month %>% group_by(EDUCATION) %>% 
+EDUCATION_Count <- credit_card %>% group_by(EDUCATION) %>% 
   summarise(
     total_count=n(),
     .groups = 'drop'
   )
 
-SEXE_Count <- default.payment.next.month %>% group_by(SEX) %>% 
+SEXE_Count <- credit_card %>% group_by(SEX) %>% 
   summarise(
     total_count=n(),
     .groups = 'drop'
   )
 
-MARRIAGE_Count <- default.payment.next.month %>% group_by(MARRIAGE) %>% 
+MARRIAGE_Count <- credit_card %>% group_by(MARRIAGE) %>% 
   summarise(
     total_count=n(),
     .groups = 'drop'
   )
 
+PAYMENT_Count <- credit_card %>% group_by(default.payment.next.month) %>% 
+  summarise(
+    total_count=n(),
+    .groups = 'drop'
+  )
+
+##################################################################
 
 education_additional_others <- EDUCATION_Count$total_count[5] + EDUCATION_Count$total_count[6] + EDUCATION_Count$total_count[7]
 education_values_list <- c(
@@ -38,8 +45,12 @@ education_labels_list <- c(
 )
 education_piepercent <- round(100 * education_values_list / sum(education_values_list), 1)
 education_labels <- paste(education_labels_list, education_piepercent, '%')
-pie(education_values_list, education_labels)
+pie(
+  education_values_list, education_labels, 
+  col = rainbow(length(education_values_list))
+)
 
+##############################
 
 sexe_values_list <- c(
   SEXE_Count$total_count[1], SEXE_Count$total_count[2]
@@ -47,8 +58,12 @@ sexe_values_list <- c(
 sexe_labels_list <- c("Male", "Female")
 sexe_piepercent <- round(100 * sexe_values_list / sum(sexe_values_list), 1)
 sexe_labels <- paste(sexe_labels_list, sexe_piepercent, '%')
-pie(sexe_values_list, sexe_labels)
+pie(
+  sexe_values_list, sexe_labels, 
+  col = rainbow(length(sexe_values_list))
+)
 
+##############################
 
 marriage_values_list <- c(
   MARRIAGE_Count$total_count[2], MARRIAGE_Count$total_count[3],
@@ -57,7 +72,48 @@ marriage_values_list <- c(
 marriage_labels_list <- c("Married", "Single", "Others")
 marriage_piepercent <- round(100 * marriage_values_list / sum(marriage_values_list), 1)
 marriage_labels <- paste(marriage_labels_list, marriage_piepercent, '%')
-pie(marriage_values_list, marriage_labels)
+pie(
+  marriage_values_list, marriage_labels,
+  col = rainbow(length(marriage_values_list))
+)
+
+##############################
+
+payment_values_list <- c(
+  PAYMENT_Count$total_count[1], PAYMENT_Count$total_count[2]
+)
+payment_labels_list <- c("Bon Payeur", "Mauvais Payeur")
+payment_piepercent <- round(100 * payment_values_list / sum(payment_values_list), 1)
+payment_labels <- paste(payment_labels_list, payment_piepercent, '%')
+pie(
+  payment_values_list, payment_labels,
+  col = rainbow(length(payment_values_list))
+)
+
+
+##################################################################
+
+Bad_Payor <- credit_card[which(credit_card$default.payment.next.month == 0),]
+Good_Payor <- credit_card[which(credit_card$default.payment.next.month == 1),]
+
+##############################
+
+marriage_table <- table(credit_card$MARRIAGE, credit_card$default.payment.next.month)
+marriage_table[8]
+
+
+marriage_values_list <- c(
+  marriage_table[6], marriage_table[7],
+  marriage_table[8]
+)
+marriage_labels_list <- c("Married", "Single", "Others")
+marriage_piepercent <- round(100 * marriage_values_list / sum(marriage_values_list), 1)
+marriage_labels <- paste(marriage_labels_list, marriage_piepercent, '%')
+pie(
+  marriage_values_list, marriage_labels,
+  col = rainbow(length(marriage_values_list)),
+  main = 'Pie Chart Bon Payeur'
+)
 
 
 cat("\014")
