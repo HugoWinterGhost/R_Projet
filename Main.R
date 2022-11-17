8f87d18921dc78f87b9ccd9ea18698658016981d
HugoWinterGhost <hfief1806@gmail.com>
2022-11-17 09:41:06 +0100

    add q2,3,4,5

--- Main.R
+++ Main.R
@@ -1,11 +1,31 @@
+################# Projet R #################
+
+############################ Question 1 et 2 ############################
+
 default.payment.next.month <- read.csv(
   "C:/UCI_Credit_Card.csv", 
   h = TRUE
-)
+  )
 
 default.payment.next.month[1:5, ]
+
 dim(default.payment.next.month)
 
+############################ Question 3 ############################
+
 summary(default.payment.next.month)
+
+############################ Question 4 ############################
+
+sapply(default.payment.next.month, mean, na.rm=TRUE)
+sapply(default.payment.next.month, sd, na.rm=TRUE)
+sapply(default.payment.next.month, median, na.rm=TRUE)
+
+############################ Question 5 ############################
+
+# install.packages("funModeling")
+library(funModeling)
+
+df_status(default.payment.next.month)
 
 cat("\014")
